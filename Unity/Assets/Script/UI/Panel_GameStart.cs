1d6f63ed977958962034fad7fc4ce2865c48e8d9
Strix <35942253+KorStrix@users.noreply.github.com>
2019-01-25 23:55:53 +0900

    게임매니져, UI 매니져 작업

---
+++ Unity/Assets/Script/UI/Panel_GameStart.cs
@@ -0,0 +1,47 @@
+﻿#region Header
+/*	============================================
+ *	작성자 : Strix
+ *	작성일 : 2019-01-25 오후 11:25:48
+ *	개요 : 
+   ============================================ */
+#endregion Header
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// 
+/// </summary>
+public class Panel_GameStart : CUGUIPanelBase
+{
+    /* const & readonly declaration             */
+
+    /* enum & struct declaration                */
+
+    /* public - Field declaration            */
+
+
+    /* protected & private - Field declaration         */
+
+
+    // ========================================================================== //
+
+    /* public - [Do] Function
+     * 외부 객체가 호출(For External class call)*/
+
+
+    // ========================================================================== //
+
+    /* protected - Override & Unity API         */
+
+
+    /* protected - [abstract & virtual]         */
+
+
+    // ========================================================================== //
+
+    #region Private
+
+    #endregion Private
+}
