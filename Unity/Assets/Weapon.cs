f8e8697661a262669b2318d3d557d44bbe348358
DESKTOP-0H74AOB\Issac Park <parkjinseo2@naver.com>
2019-01-25 23:24:41 +0900

    item&weapon

---
+++ Unity/Assets/Weapon.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+    /// <summary>
+    /// 무기의 이름
+    /// </summary>
+    public string Name;
+
+    /// <summary>
+    /// 무기가 입히는 피해
+    /// </summary>
+    public float Damage;
+
+    /// <summary>
+    /// 무기의 사거리
+    /// </summary>
+    public float Range;
+
+    /// <summary>
+    /// 무기의 쿨타임
+    /// </summary>
+    public float Cooltime;
+
+    /// <summary>
+    /// 남은 탄알 수(-1은 무한)
+    /// </summary>
+    public int Ammo;
+
+    public void Attach(Transform character) {
+        transform.parent = character;
+        transform.localPosition = Vector3.zero;
+    }
+    public void Detach() {
+        transform.SetParent(null, true);
+    }
+}
