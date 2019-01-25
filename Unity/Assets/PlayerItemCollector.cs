ebeb742c82ceef8873f8671a5a119db4649248ac
DESKTOP-0H74AOB\Issac Park <parkjinseo2@naver.com>
2019-01-26 02:51:57 +0900

    item collector

--- Unity/Assets/PlayerItemCollector.cs
+++ Unity/Assets/PlayerItemCollector.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerItemCollector : MonoBehaviour {
-    Weapon weaponHeld;
-    Armor armorHeld;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+
+    public delegate void OnGetWeapon(Weapon newWeapon);
+    public delegate void OnGetArmor(Armor newArmor);
+    public delegate void OnGetJewel(Jewel newJewel);
+
+    public event OnGetWeapon EventOnGetWeapon;
+    public event OnGetArmor EventOnGetArmor;
+    public event OnGetJewel EventOnGetJewel;
+
+    public Weapon WeaponHeld { get; private set; }
+    public Armor ArmorHeld { get; private set; }
+    public Jewel JewelHeld { get; private set; }
+    // Use this for initialization
+    void Start () {
+
+    }
+
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
@@ -21,25 +33,42 @@ public class PlayerItemCollector : MonoBehaviour {
     /// <param name="other"></param>
     private void OnTriggerEnter(Collider other) {
 
-        Debug.Log("TODO : 아이템을 줍는 코드 삽입");
+       
+
         switch (other.tag) {
-
-
-        }
-        var h = other.GetComponent<Holdable>();
-        if (h != null) {
+        case "weapon":
+            var h = other.GetComponent<Holdable>();
             h.Attach(transform);
-            if (weaponHeld != null)
-                weaponHeld.GetComponent<Holdable>().Detach();
-            weaponHeld = h.GetComponent<Weapon>();
+            if (WeaponHeld != null)
+                WeaponHeld.GetComponent<Holdable>().Detach();
+            WeaponHeld = h.GetComponent<Weapon>();
+            EventOnGetWeapon(WeaponHeld);
+            break;
+        case "armor":
+            var h2 = other.GetComponent<Holdable>();
+            h2.Attach(transform);
+            if (ArmorHeld != null)
+                ArmorHeld.GetComponent<Holdable>().Detach();
+            ArmorHeld = h2.GetComponent<Armor>();
+            EventOnGetArmor(ArmorHeld);
+            break;
+        case "consumable":
+            var c = other.GetComponent<Consumable>();
+            Destroy(other.gameObject);
+            break;
+        case "jewel":
+            var h3 = other.GetComponent<Holdable>();
+            h3.Attach(transform);
+            if (JewelHeld != null)
+                JewelHeld.GetComponent<Holdable>().Detach();
+            JewelHeld = h3.GetComponent<Jewel>();
+            EventOnGetJewel(JewelHeld);
+            break;
         }
+        
+           
+        
 
-        var c = other.GetComponent<Consumable>();
-        if (c != null) {
-            var e = c.Effects;
-            //stat read
-            //GetComponentInParent<CharacterModel>().basics.
-
-        }
+        
     }
 }
